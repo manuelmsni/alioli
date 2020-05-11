30e4aa7286744a630aea70ff3de68e018f356bc4
manuelmsni <manuelmsni@hotmail.com>
2020-05-11 21:34:08 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
       layoutMode: 'fitRows'
     });
     
-     $('#option-flters li').on('load', function() {
+     $(window).on('load', function() {
       $("#option-flters li").removeClass('filter-active');
       $('#sin-filtro').addClass('filter-active');
       optionIsotope.isotope({
