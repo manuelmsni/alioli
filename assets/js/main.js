00536d0e67599e9388a37622bbc142d6ab404ad5
manuelmsni <manuelmsni@hotmail.com>
2020-05-11 21:15:43 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,14 @@ cookieOptions = {msg: "Además de hacerlas, nuestra web usa Cookies", close: "Lo
   // Ventana modal en construcción
 $( document ).ready(function() {
     $('#myModal').modal('toggle')
+     $("#option-flters li").removeClass('filter-active');
+      $(#sin-filtro).addClass('filter-active');
 });
 
 
 !(function($) {
   "use strict";
   
-    // aplicar primera opción al cargar
-$( document ).ready(function() {
-   $("#option-flters li").removeClass('filter-active');
-      $(#sin-filtro).addClass('filter-active');
-      optionIsotope.isotope({
-        filter: $(#sin-filtro).data('filter')
-      });
-});
     // filtro option entradas
   $(window).on('load', function() {
     var optionIsotope = $('.option-container').isotope({
